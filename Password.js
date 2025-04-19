3fab57ca399673ec17895ed0a3042f3a030dd55d
jejep099 <agastrastore@gmail.com>
2025-04-19 21:09:32 +0700

    Create Password.js

---
+++ Password.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "jejepganteng",
+
+  "description": "agastra crash v.5 vip", 
+
+  "lastUpdated": "12/04/2025",
+
+  "author": "jejep", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "5.0"
+
+}
