ab4f030f19a410535f680ec2ae50da09d3771889
jejep099 <agastrastore@gmail.com>
2025-04-20 11:02:46 +0700

    Update Password.js

--- Password.js
+++ Password.js
@@ -4,14 +4,6 @@
 
   "description": "agastra crash v.5 vip", 
 
-  "lastUpdated": "12/04/2025",
-
-  "author": "jejep", 
-
-  "status": "Active",
-
-  "license": "MIT",
-
-  "version": "5.0"
+  "creator by": "agastra jejep""
 
 }
